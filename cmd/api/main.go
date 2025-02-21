9f8fc63d942fee14b67da1d37af7f075370e40d0
Anton Kraev <antonkraeww@gmail.com>
2025-02-21 23:24:39 +0300

    feat: api entrypoint and config (yaml+.env)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,34 +1,95 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	bolt "go.etcd.io/bbolt"
+
 	"github.com/Anton-Kraev/gopay"
+	"github.com/Anton-Kraev/gopay/internal/client/yookassa"
+	"github.com/Anton-Kraev/gopay/internal/config"
 	"github.com/Anton-Kraev/gopay/internal/http/handler"
 	"github.com/Anton-Kraev/gopay/internal/http/server"
+	"github.com/Anton-Kraev/gopay/internal/links"
 	"github.com/Anton-Kraev/gopay/internal/logger"
+	repo "github.com/Anton-Kraev/gopay/internal/repository/bolt"
+	"github.com/Anton-Kraev/gopay/internal/templates"
 	"github.com/Anton-Kraev/gopay/internal/validator"
 	"github.com/Anton-Kraev/gopay/mock"
 )
 
+const (
+	configPath = "./configs/api.yaml"
+)
+
 func main() {
+	cfg, err := config.GetConfig(configPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(cfg)
+
+	db, err := bolt.Open(
+		cfg.DB.FilePath,
+		0600,
+		&bolt.Options{Timeout: cfg.DB.OpenTimeout},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer func(db *bolt.DB) {
+		if err = db.Close(); err != nil {
+			log.Println("error closing db connection:", err)
+		}
+	}(db)
+
+	paymentStorage, err := repo.NewPaymentRepository(db)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	paymentService := yookassa.NewClient(cfg.Yookassa.CheckoutURL, yookassa.AuthConfig{
+		ID:    cfg.Yookassa.ShopID,
+		Token: cfg.Yookassa.APIToken,
+	})
+
+	linkGenerator := links.NewGenerator(fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port))
+
+	templateStorage, err := templates.New(db)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err = templateStorage.SetTemplate("template", gopay.PaymentTemplate{
+		Currency:     "RUB",
+		Amount:       1,
+		Description:  "description",
+		ResourceLink: "http://127.0.0.1:8080/file/123",
+	}); err != nil {
+		log.Fatalln(err)
+	}
+
 	pm := gopay.NewPaymentManager(
-		mock.NewTemplates(),
-		mock.NewLinkGenerator(),
-		mock.NewPaymentStorage(),
-		mock.NewPaymentService(),
+		templateStorage,
+		linkGenerator,
+		paymentStorage,
+		paymentService,
 	)
 
 	fileStorage := mock.FileStorage{}
 
-	hndl := handler.NewHandler(pm, nil, fileStorage)
+	hndl := handler.NewHandler(pm, fileStorage)
 
-	log := logger.Setup("local")
 	val, err := validator.NewValidator()
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	srv := server.NewServer(hndl, log, val)
+	srv := server.NewServer(hndl, logger.Setup(cfg.Env), val)
 
 	echoSrv := srv.InitRoutes()
-	echoSrv.Logger.Fatal(echoSrv.Start(":1323"))
+	echoSrv.Logger.Fatal(echoSrv.Start(":" + cfg.Server.Port))
 }
---
+++ internal/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+var (
+	once     sync.Once
+	instance Config
+)
+
+type Config struct {
+	Env string `yaml:"env"`
+
+	Server struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	} `yaml:"server"`
+
+	DB struct {
+		FilePath    string        `yaml:"file_path"`
+		OpenTimeout time.Duration `yaml:"open_timeout"`
+	} `yaml:"db"`
+
+	Yookassa struct {
+		CheckoutURL string `env:"YOOKASSA_CHECKOUT_URL"`
+		ShopID      string `env:"YOOKASSA_SHOP_ID"`
+		APIToken    string `env:"YOOKASSA_API_TOKEN"`
+	}
+}
+
+func GetConfig(path string) (Config, error) {
+	var err error
+
+	once.Do(func() {
+		err = errors.Join(
+			cleanenv.ReadConfig(path, &instance),
+			godotenv.Load(),
+			cleanenv.ReadEnv(&instance),
+		)
+	})
+
+	return instance, err
+}
